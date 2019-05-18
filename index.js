fcb9eda2991d91dbec6b0391229f312bfad36a9d
AlexeyCherneny <a.cherneny@gmail.com>
2019-05-18 18:49:25 +0300

    Add gitignore

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ require('babel-register')({
   presets: ['env'],
 });
 // Import the rest of our application.
-module.exports = require('./index.js');
+module.exports = require('./src/index.js');
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-console */
-
 import express from 'express';
 import dbConfig from './config/db';
 import middleWaresConfig from './config/middlewares';
